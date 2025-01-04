3126ea16dcec146b93715ae7234932d5b90b4c2d
Jatin Jindal <jatin.jindal@timesinternet.in>
2025-01-05 01:31:45 +0530

    Handling all warnings

--- src/main/java/org/spark/tutorial/Main.java
+++ src/main/java/org/spark/tutorial/Main.java
@@ -5,8 +5,8 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,32 +15,29 @@ public class Main {
                 .appName("Simple Application")
                 .getOrCreate();
 
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        try (JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext())) {
+            JavaRDD<dataSchema> data = jsc.textFile("src/main/resources/data.txt")
+                    .zipWithIndex()
+                    .filter(line -> line._2() > 2)
+                    .map(Tuple2::_1)
+                    .filter(line -> !line.isEmpty())
+                    .map(line -> {
+                        String[] parts = line.split(" +");
+                        dataSchema dataSchema = new dataSchema();
+                        try {
+                            dataSchema.setName(parts[0]);
+                            dataSchema.setVal(parts[1]);
+                        } catch (Exception e) {
+                            System.out.println("Error: " + e);
+                        }
+                        return dataSchema;
+                    });
 
-        JavaRDD<dataSchema> data = jsc.textFile("src/main/resources/data.txt")
-                .zipWithIndex()
-                .filter(line -> line._2()>2)
-                .map(line -> line._1())
-                .filter(line -> !line.isEmpty())
-                .map(line -> {
-                    String[] parts = line.split("\s+");
-                    dataSchema dataSchema = new dataSchema();
-                    try {
-                        dataSchema.setName(parts[0]);
-                        dataSchema.setVal(parts[1]);
-                    }
-                    catch (Exception e) {
-                        System.out.println("Error: " + e);
-                    }
-                    return dataSchema;
-                });
+            Dataset<dataSchema> ds = spark.createDataset(data.rdd(), Encoders.bean(dataSchema.class));
 
-        Dataset<dataSchema> ds = spark.createDataset(data.rdd(), Encoders.bean(dataSchema.class));
+            ds.show(10, false);
 
-        ds.show(10,false);
-
-        spark.stop();
-
-
+            spark.stop();
+        }
     }
 }
