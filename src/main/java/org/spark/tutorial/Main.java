969abcb67fdd31902f5ea3ad34412dedb43fc90e
Jatin Jindal <jatin.jindal@timesinternet.in>
2025-01-05 01:19:49 +0530

    Way To Read Text File Via Spark in Java

--- src/main/java/org/spark/tutorial/Main.java
+++ src/main/java/org/spark/tutorial/Main.java
@@ -1,7 +1,46 @@
 package org.spark.tutorial;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SparkSession spark = SparkSession.builder()
+                .master("local")
+                .appName("Simple Application")
+                .getOrCreate();
+
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+        JavaRDD<dataSchema> data = jsc.textFile("src/main/resources/data.txt")
+                .zipWithIndex()
+                .filter(line -> line._2()>2)
+                .map(line -> line._1())
+                .filter(line -> !line.isEmpty())
+                .map(line -> {
+                    String[] parts = line.split("\s+");
+                    dataSchema dataSchema = new dataSchema();
+                    try {
+                        dataSchema.setName(parts[0]);
+                        dataSchema.setVal(parts[1]);
+                    }
+                    catch (Exception e) {
+                        System.out.println("Error: " + e);
+                    }
+                    return dataSchema;
+                });
+
+        Dataset<dataSchema> ds = spark.createDataset(data.rdd(), Encoders.bean(dataSchema.class));
+
+        ds.show(10,false);
+
+        spark.stop();
+
+
     }
 }
--- src/main/java/org/spark/tutorial/dataSchema.java
+++ src/main/java/org/spark/tutorial/dataSchema.java
@@ -1,4 +1,32 @@
 package org.spark.tutorial;
 
-public class dataSchema {
+import java.io.Serializable;
+
+public class dataSchema implements Serializable {
+    private String name;
+    private String val;
+
+    public dataSchema(String name, String val) {
+        this.name = name;
+        this.val = val;
+    }
+
+    public dataSchema() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getVal() {
+        return this.val;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
 }
